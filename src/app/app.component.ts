82ee375242a5657632e3c94cc63b3ccdd6ec7ad1
Jose Manuel <56078203+Makoy521@users.noreply.github.com>
2024-06-24 15:48:20 -0700

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bases';
+  success= false;
+  passwordResetSucces=false;
+  authenticationError=false;
+  fehcaError=false;
+  userUnlock=false;
+  username="";
+  password="";
+  timeLapse = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.timeLapse = true;
+    }, 3000);
+  }
+
+  cerrarSesiones(){
+
+  }
+  login(){
+    this.success = false;
+    this.passwordResetSucces = false;
+    this.authenticationError = false;
+    this.fehcaError = false;
+    this.userUnlock = false; // Inicialmente todos los errores desactivados
+
+    // Activar un error aleatoriamente
+    const randomNumber = Math.floor(Math.random() * 5); // Genera un número aleatorio entre 0 y 3
+
+    switch (randomNumber) {
+      case 0:
+        this.success = true;
+        break;
+      case 1:
+        this.passwordResetSucces = true;
+        break;
+      case 2:
+        this.authenticationError = true;
+        break;
+      case 3:
+        this.fehcaError = true;
+        break;
+      case 4:
+        this.userUnlock = true;
+        break;
+      default:
+        break;
+    }
+  }
+  requestResetPassword(){
+
+  }
 }
